277ddc649c6d264de5f5cb8023b546d0acb8c7b9
Kamsi Kodi <kamsikodi@gmail.com>
2021-03-26 10:14:23 +0100

    Worked on UI

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAMjIKVDC3YBRKR_pgn7wRpzxGL7jHqfhU",
+    authDomain: "fir-todo-c38e6.firebaseapp.com",
+    databaseURL: "https://fir-todo-c38e6-default-rtdb.firebaseio.com", 
+    projectId: "fir-todo-c38e6",
+    storageBucket: "",
+    messagingSenderId: "722706665287",
+    appId: "1:722706665287:web:a14ec498f0e1ac7351c662"
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,168 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-form @submit.prevent="handleSubmit">
+    <v-container style='display:flex; position:absolute; top:0; bottom:0; right:0; left:0;'>
+      <v-row style="align-items: center;margin: auto;">
+        <v-col
+          cols="4"
+          md="6"
+        >
+        <v-card
+        class="mx-auto"
+        max-width="400"
+        tile
+      >
+      <v-subheader>INDEXED DB STORAGE ANALYSIS</v-subheader>
+      <v-divider></v-divider>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>Time to sort data</v-list-item-title>
+            <v-list-item-subtitle>12 seconds</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>Push Time</v-list-item-title>
+            <v-list-item-subtitle>10 seconds</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>Time to accept new data</v-list-item-title>
+            <v-list-item-subtitle>15 seconds</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+      </v-col>
+        <v-col
+          cols="8"
+          md="6"
+        >
+          <v-row>
+            <v-col
+              cols="12"
+              md="12"
+            >
+              <v-text-field
+                v-model="name"
+                label="Name"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col
+              cols="12"
+              md="12"
+            >
+              <v-text-field
+                v-model="email"
+                label="Email"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col
+              cols="12"
+              md="12"
+            >
+              <v-text-field
+                v-model="phone"
+                label="Phone"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col
+              cols="12"
+              md="12"
+            >
+              <v-text-field
+                v-model="dob"
+                label="Dob"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col
+              cols="12"
+              md="12"
+            >
+              <v-text-field
+                v-model="address"
+                label="Address"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col
+              cols="12"
+              md="12"
+            >
+              <v-text-field
+                v-model="bvn"
+                label="Bvn"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row style="justify-content: center;">
+            <v-btn
+                type="submit"
+                depressed
+                style="color: #fff;background-color: #419789;padding: 25px;"
+              >
+              Submit
+            </v-btn>
+          </v-row>
+        </v-col>
+      </v-row>
+
+    </v-container>
+  </v-form>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import db from '../firebase/firebaseInit';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      name: '',
+      email: '',
+      phone: '',
+      dob: '',
+      address: '',
+      bvn: ''
+    }
+  },
+  methods: {
+    handleSubmit() {
+      console.log(true);
+    }
+  },
+  created() {
+    const start = new Date().getTime();
+    
+    setTimeout(function () {
+      const end = new Date().getTime();
+      const diff = end - start;
+      const seconds = Math.floor(diff / 1000 % 60);
+      console.log(seconds);
+    }, 5000);
+    // db.collection('demo_data').get().then(snap => {
+    //   var size = snap.size;
+    //   console.log(size);
+    //   const end = new Date().getTime();
+    //   const diff = end - start;
+    //   const seconds = Math.floor(diff / 1000 % 60);
+    //   console.log(seconds);
+    // });
   }
 }
 </script>
